da0666c8fa1bfff2c0a13fc7406e8600f668e517
A601-10 <tnk.dat.class@gmail.com>
2021-06-10 10:01:31 +0900

    Initial commit

---
+++ fukuvHensu/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuvHensu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Left += int.Parse(textBox1.Text);
+            label2.Top += int.Parse(textBox2.Text);
+            //label2.Text =  ""+  (int.Parse(textBox1.Text)) + (int.Parse(textBox2.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "-5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-5";
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+            textBox2.Text = "0";
+        }
+    }
+}
